af5e8ec2aac42720b18f000bc8a836f6c65eefa2
yulapshun <yulapshun@gmail.com>
2016-09-22 23:12:07 +0800

    Install magit

--- init.el
+++ init.el
@@ -100,7 +100,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (magit web-mode solarized-theme neotree highlight-symbol evil auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
