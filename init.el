444cf0f090eaba9a7f5ec73f8b38e74e6a420bbe
yulapshun <yulapshun@gmail.com>
2017-02-15 15:55:37 +0800

    Enable windmove

--- init.el
+++ init.el
@@ -102,6 +102,14 @@
 ;; flx-ido
 (flx-ido-mode 1)
 
+;; windmove
+(windmove-default-keybindings)
+;; Fix conflict with org-mode
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 ;; eval and replace
 (defun eval-and-replace (value)
   "Evaluate the sexp at point and replace it with its value"
