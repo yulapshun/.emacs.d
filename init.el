b6612b465450614a5b80ffe29ab67050d593800f
yulapshun <yulapshun@gmail.com>
2017-03-19 10:26:53 +0800

    Update packages

--- init.el
+++ init.el
@@ -123,6 +123,10 @@
 (require 'uniquify)
   (setq uniquify-buffer-name-style 'forward)
 
+;; org-mode
+;; syntax highlight in code blocks
+(setq org-src-fontify-natively t)
+
 ;; eval and replace
 (defun eval-and-replace (value)
   "Evaluate the sexp at point and replace it with its value"
@@ -157,7 +161,7 @@ ARG defaults to 79"
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (flx-ido magit web-mode solarized-theme neotree highlight-symbol evil auto-complete))))
+    (yaml-mode flx-ido magit web-mode solarized-theme neotree highlight-symbol evil auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
