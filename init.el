5465e4167984c53ec141bd1a412a0a3b4c34327d
yulapshun <yulapshun@gmail.com>
2017-04-13 21:53:42 +0800

    Install flycheck

--- init.el
+++ init.el
@@ -132,6 +132,9 @@
 (setq emojify-emoji-styles '(unicode))
 (add-hook 'after-init-hook #'global-emojify-mode)
 
+;; flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
 ;; eval and replace
 (defun eval-and-replace (value)
   "Evaluate the sexp at point and replace it with its value"
@@ -166,7 +169,7 @@ ARG defaults to 79"
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (emojify go-mode yaml-mode flx-ido magit web-mode solarized-theme neotree highlight-symbol evil auto-complete))))
+    (flycheck emojify go-mode yaml-mode flx-ido magit web-mode solarized-theme neotree highlight-symbol evil auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
