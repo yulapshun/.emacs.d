6d5173206262839ae0705055d6be04f3694915f5
yulapshun <yulapshun@gmail.com>
2017-08-11 11:47:47 +0800

    Update packages

--- elpa/js2-mode-20170721/js2-mode-autoloads.el
+++ elpa/js2-mode-20170721/js2-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "js2-imenu-extras" "js2-imenu-extras.el" (22803
-;;;;;;  6676 346503 100000))
+;;;### (autoloads nil "js2-imenu-extras" "js2-imenu-extras.el" (22925
+;;;;;;  9003 0 0))
 ;;; Generated autoloads from js2-imenu-extras.el
 
 (autoload 'js2-imenu-extras-setup "js2-imenu-extras" "\
@@ -19,8 +19,7 @@ Toggle Imenu support for frameworks and structural patterns.
 
 ;;;***
 
-;;;### (autoloads nil "js2-mode" "js2-mode.el" (22803 6676 473167
-;;;;;;  342000))
+;;;### (autoloads nil "js2-mode" "js2-mode.el" (22925 9003 0 0))
 ;;; Generated autoloads from js2-mode.el
 
 (autoload 'js2-highlight-unused-variables-mode "js2-mode" "\
@@ -56,7 +55,7 @@ variables (`sgml-basic-offset' et al) locally, like so:
 ;;;***
 
 ;;;### (autoloads nil nil ("js2-mode-pkg.el" "js2-old-indent.el")
-;;;;;;  (22803 6676 429834 838000))
+;;;;;;  (22925 9003 0 0))
 
 ;;;***
 
--- elpa/with-editor-2.5.11/with-editor-autoloads.el
+++ elpa/with-editor-2.5.11/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22683 15114 159129 275000))
+;;;;;;  (22925 8995 0 0))
 
 ;;;***
 
