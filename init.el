b9f74e4cdf3892154445a9f327109a2beb64eba2
yulapshun <yulapshun@gmail.com>
2017-08-21 18:24:16 +0800

    Install eyebrowse

--- init.el
+++ init.el
@@ -148,6 +148,9 @@
 ;; auto revert
 (global-auto-revert-mode 1)
 
+;; eyebrowse-mode
+(eyebrowse-mode t)
+
 ;; eval and replace
 (defun eval-and-replace (value)
   "Evaluate the sexp at point and replace it with its value"
@@ -182,7 +185,7 @@ ARG defaults to 79"
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (avy js2-mode php-mode projectile flycheck emojify go-mode yaml-mode flx-ido magit web-mode solarized-theme neotree highlight-symbol evil auto-complete))))
+    (eyebrowse avy js2-mode php-mode projectile flycheck emojify go-mode yaml-mode flx-ido magit web-mode solarized-theme neotree highlight-symbol evil auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
