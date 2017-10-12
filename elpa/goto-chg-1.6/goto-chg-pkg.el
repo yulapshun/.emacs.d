c813232197d46e439bec0260fd19519c0b1cd6be
yulapshun <yulapshun@gmail.com>
2017-10-12 11:59:31 +0800

    Update packages

--- elpa/goto-chg-1.6/goto-chg-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "goto-chg" "1.6" "goto last change" 'nil :authors '(("David Andersson <l.david.andersson(at)sverige.nu>")) :maintainer '("David Andersson <l.david.andersson(at)sverige.nu>"))
