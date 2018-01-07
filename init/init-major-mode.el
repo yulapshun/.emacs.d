af762a31f91625d55208a0860775cf0e9e6eb6b6
yulapshun <yulapshun@gmail.com>
2018-01-07 14:10:07 +0800

    Install jsx mode

--- init/init-major-mode.el
+++ init/init-major-mode.el
@@ -9,7 +9,8 @@
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
 
 (provide 'init-major-mode)
