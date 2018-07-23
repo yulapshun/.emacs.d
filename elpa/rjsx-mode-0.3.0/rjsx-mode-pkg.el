346b584fe53c034a8f50bc1ee8d29b17d02eb97e
yulapshun <yulapshun@gmail.com>
2018-07-23 10:27:38 +0800

    Update packages

--- elpa/rjsx-mode-0.3.0/rjsx-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rjsx-mode" "0.3.0" "Real support for JSX" '((emacs "24.4") (js2-mode "20170504")) :commit "79bedb1d188e209be5cfef3bea8906df9c509ab8" :url "https://github.com/felipeochoa/rjsx-mode/" :keywords '("languages"))
