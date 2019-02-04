fc02b0d11fd4884d43ad02a1eaf51a9c11c0498f
yulapshun <yulapshun@gmail.com>
2019-02-04 11:09:02 +0800

    use web-mode instead of jinja2 mode

--- init/init-major-mode.el
+++ init/init-major-mode.el
@@ -9,6 +9,7 @@
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jinja2\\'" . web-mode))
 (add-hook 'web-mode-hook
           (lambda ()
             (setq web-mode-markup-indent-offset 2)
