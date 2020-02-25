e2fb4b40bad9e52b2beb23e18a5adbb260e2b44a
yulapshun <yulapshun@gmail.com>
2020-02-25 13:51:35 +0800

    Update packages

--- emacs-custom.el
+++ emacs-custom.el
@@ -5,14 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "a5956ec25b719bf325e847864e16578c61d8af3e8a3d95f60f9040d02497e408" "8a6f10b3269c42c351776ee32c7ee755472dee4ecdf2177dcbf64d5e02d1ab64" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "a5956ec25b719bf325e847864e16578c61d8af3e8a3d95f60f9040d02497e408" "8a6f10b3269c42c351776ee32c7ee755472dee4ecdf2177dcbf64d5e02d1ab64" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(default-input-method "chinese-b5-tsangchi")
  '(lsp-auto-guess-root t)
  '(lsp-ui-imenu-enable t)
  '(lsp-ui-sideline-enable nil)
  '(package-selected-packages
    (quote
-    (yasnippet ido-completing-read+ company-flx company-lsp lsp-mode git-gutter company-web tern pug-mode symbol-overlay anaconda-mode company-anaconda company rjsx-mode jsx-mode dockerfile-mode exec-path-from-shell geiser paredit eyebrowse qml-mode avy js2-mode php-mode projectile flycheck emojify go-mode yaml-mode flx-ido magit web-mode solarized-theme neotree highlight-symbol evil)))
+    (yasnippet ido-completing-read+ company-flx company-lsp lsp-mode git-gutter company-web tern pug-mode symbol-overlay anaconda-mode company-anaconda company rjsx-mode jsx-mode dockerfile-mode exec-path-from-shell geiser paredit eyebrowse qml-mode avy js2-mode php-mode flycheck emojify go-mode yaml-mode flx-ido web-mode solarized-theme neotree highlight-symbol evil)))
  '(safe-local-variable-values
    (quote
     ((web-mode-engines-alist
