7acf4bb5c4be1490674177bbe68fd69e70578a08
yulapshun <yulapshun@gmail.com>
2020-04-30 22:11:19 +0800

    Add hooks to lsp-mode

--- init/init-lsp-mode.el
+++ init/init-lsp-mode.el
@@ -3,30 +3,29 @@
 (require 'lsp-ui)
 
 (custom-set-variables
- '(lsp-auto-guess-root t)
+ '(lsp-auto-guess-root nil)
  '(lsp-ui-sideline-enable nil)
  '(lsp-ui-imenu-enable t)
  '(lsp-ui-peek-always-show t)
  '(lsp-ui-doc-enable nil))
 
-;; Hook javascript modes
-;; Install server with the following command, which supports both
-;; javascript and typescript
 ;; npm i -g typescript-language-server
+;; pip install python-language-server
+;; npm i -g vscode-css-languageserver-bin
+;; npm i -g vscode-html-languageserver-bin
+
 (add-hook 'js-mode-hook #'lsp)
 (add-hook 'js2-mode-hook #'lsp)
 (add-hook 'rjsx-mode-hook #'lsp)
-
-;; Hook python modes
-;; Install with the following command
-;; pip install python-language-server
 (add-hook 'python-mode-hook #'lsp)
+(add-hook 'web-mode-hook #'lsp)
+(add-hook 'css-mode-hook #'lsp)
 
 
 ;; Define key bindings
 (let ((map (make-sparse-keymap)))
   (define-key map (kbd "C-c l .") #'lsp-find-definition)
-  (define-key map (kbd "C-c l ?") #'lsp-find-references)
+  (define-key map (kbd "C-c l /") #'lsp-find-references)
   (define-key map (kbd "C-c l d") #'lsp-ui-doc-glance)
   (define-key map (kbd "C-c l i") #'lsp-ui-peek-find-implementation)
   (define-key map (kbd "C-c l I") #'lsp-find-implementation)
