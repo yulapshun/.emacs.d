4b1e475020b3423306630c757713b59a6d2d6fd7
yulapshun <yulapshun@gmail.com>
2020-04-30 13:03:39 +0800

    Updaet packages

--- elpa/lv-20200122.1200/lv-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lv" "20200122.1200" "Other echo area" 'nil :commit "d3328cab67714fbc164781d7bbe0f9d150f2e9a3" :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel"))
