b232ecff48b584ab2d535f8574aff24996d6e2ad
yulapshun <yulapshun@gmail.com>
2020-05-07 16:31:39 +0800

    Fix typo

--- init/init-major-mode.el
+++ init/init-major-mode.el
@@ -19,7 +19,7 @@
   (add-hook 'js2-mode-hook
             (lambda ()
               (setq js2-basic-offset 2))))
-(add-hook 'js-mode
+(add-hook 'js-mode-hook
           (lambda ()
             (setq js-indent-level 2)))
 (add-hook 'css-mode-hook
