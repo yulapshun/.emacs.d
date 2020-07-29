79b3ffb686755602318d0f4bddc26ed0ea095852
yulapshun <yulapshun@gmail.com>
2020-07-29 17:36:37 +0800

    Set web mode indent offset

--- init/init-web-mode.el
+++ init/init-web-mode.el
@@ -2,5 +2,6 @@
 (setq web-mode-enable-auto-indentation nil)
 (setq web-mode-enable-engine-detection t)
 (setq web-mode-engines-alist '(("django" . "\\.jinja2\\'")))
+(setq web-mode-markup-indent-offset 2)
 
 (provide 'init-web-mode)
