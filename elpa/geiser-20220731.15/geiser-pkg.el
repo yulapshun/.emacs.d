8785ef8cec6506631d0f943e112018b86b6d613c
yulapshun <yulapshun@gmail.com>
2022-08-01 11:40:37 +0200

    Update packages

---
+++ elpa/geiser-20220731.15/geiser-pkg.el
@@ -0,0 +1,14 @@
+(define-package "geiser" "20220731.15" "GNU Emacs and Scheme talk to each other"
+  '((emacs "25.1")
+    (transient "0.3")
+    (project "0.8.1"))
+  :commit "2fac53cb84e1341fd838ffdd2d9f7e09ecaf617a" :authors
+  '(("Jose Antonio Ortega Ruiz" . "jao@gnu.org"))
+  :maintainer
+  '("Jose Antonio Ortega Ruiz" . "jao@gnu.org")
+  :keywords
+  '("languages" "scheme" "geiser")
+  :url "https://gitlab.com/emacs-geiser/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/gruvbox-theme-20220731.1400/gruvbox-theme-pkg.el
@@ -0,0 +1,10 @@
+(define-package "gruvbox-theme" "20220731.1400" "A retro-groove colour theme for Emacs"
+  '((autothemer "0.2"))
+  :commit "d5ab9e2390b077c32d66f92ac502cb1c943c5a52" :authors
+  '(("Jason Milkins" . "jasonm23@gmail.com"))
+  :maintainer
+  '("Jason Milkins" . "jasonm23@gmail.com")
+  :url "http://github.com/greduan/emacs-theme-gruvbox")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
