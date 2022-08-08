66ed1f8eb30bf48105175562d72ab82038380ad6
yulapshun <yulapshun@gmail.com>
2022-08-08 07:51:39 +0200

    Install all-the-icons

--- custom.el
+++ custom.el
@@ -3,11 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
  '(custom-safe-themes
-   '("7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" default))
+   '("521682d356435276b4bfb60cd134681aeaf4f2e4ee625456c04285da725ebf7c" default))
  '(package-selected-packages
-   '(yaml-mode web-mode use-package undo-tree typescript-mode symbol-overlay solarized-theme qml-mode pythonic pug-mode projectile php-mode paredit neotree magit-popup lsp-ui lsp-java ido-completing-read+ gruvbox-theme graphql goto-chg go-mode git-gutter ghub geiser fringe-helper flycheck flx-ido eyebrowse exec-path-from-shell emojify elisp-benchmarks dockerfile-mode dash-functional company-web company-flx auto-compile async)))
+   '(yasnippet yaml-mode web-mode use-package undo-tree typescript-mode symbol-overlay solarized-theme rjsx-mode qml-mode pythonic pug-mode php-mode paredit neotree magit-popup lsp-ui lsp-java ido-completing-read+ gruvbox-theme graphql goto-chg go-mode git-gutter ghub geiser fringe-helper flycheck flx-ido eyebrowse exec-path-from-shell emojify elisp-benchmarks dockerfile-mode dash-functional company-web company-flx auto-compile async all-the-icons-dired)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
