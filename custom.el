ca8da64d94089705a70e927b8aec910cae3fc6f1
yulapshun <yulapshun@gmail.com>
2022-08-08 21:14:38 +0200

    Customize org-mode style

--- custom.el
+++ custom.el
@@ -13,4 +13,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:inherit default :weight bold :height 1.5 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :height 1.3))))
+ '(org-level-2 ((t (:inherit default :weight bold :height 1.2))))
+ '(org-level-3 ((t (:inherit default :weight bold :height 1.1))))
+ '(org-level-4 ((t (:inherit default :weight bold))))
+ '(org-level-5 ((t (:inherit default :weight bold))))
+ '(org-level-6 ((t (:inherit default :weight bold))))
+ '(org-level-7 ((t (:inherit default :weight bold))))
+ '(org-level-8 ((t (:inherit default :weight bold)))))
