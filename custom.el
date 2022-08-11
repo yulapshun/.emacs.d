68c38f2c823d98b6c23d5550eebfabcaca568ec5
yulapshun <yulapshun@gmail.com>
2022-08-11 23:25:18 +0200

    Install vterm

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("969a67341a68becdccc9101dc87f5071b2767b75c0b199e0ded35bd8359ecd69" "521682d356435276b4bfb60cd134681aeaf4f2e4ee625456c04285da725ebf7c" default))
  '(package-selected-packages
-   '(avy benchmark-init esup dashboard undo-tree pyvenv pyvenv-auto ido-vertical-mode yasnippet-snippets company-box vundo yasnippet yaml-mode web-mode use-package symbol-overlay solarized-theme rjsx-mode pug-mode projectile php-mode paredit org-superstar neotree magit lsp-ui ido-completing-read+ gruvbox-theme go-mode git-gutter flycheck flx-ido eyebrowse exec-path-from-shell emojify elisp-benchmarks dockerfile-mode company-web company-flx auto-compile all-the-icons-dired))
+   '(vterm avy benchmark-init esup dashboard undo-tree pyvenv pyvenv-auto ido-vertical-mode yasnippet-snippets company-box vundo yasnippet yaml-mode web-mode use-package symbol-overlay solarized-theme rjsx-mode pug-mode projectile php-mode paredit org-superstar neotree magit lsp-ui ido-completing-read+ gruvbox-theme go-mode git-gutter flycheck flx-ido eyebrowse exec-path-from-shell emojify elisp-benchmarks dockerfile-mode company-web company-flx auto-compile all-the-icons-dired))
  '(warning-suppress-types '((comp))))
 
 (custom-set-faces
@@ -14,4 +14,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:inherit default :weight bold :height 1.5 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :height 1.3))))
+ '(org-level-2 ((t (:inherit default :weight bold :height 1.2))))
+ '(org-level-3 ((t (:inherit default :weight bold :height 1.1))))
+ '(org-level-4 ((t (:inherit default :weight bold))))
+ '(org-level-5 ((t (:inherit default :weight bold))))
+ '(org-level-6 ((t (:inherit default :weight bold))))
+ '(org-level-7 ((t (:inherit default :weight bold))))
+ '(org-level-8 ((t (:inherit default :weight bold)))))
