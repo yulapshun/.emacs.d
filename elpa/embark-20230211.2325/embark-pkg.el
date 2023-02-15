0f91b5f7f88b8e2e51b5e5997c89d3642824a110
yulapshun <yulapshun@gmail.com>
2023-02-15 17:46:26 +0100

    Update packages

---
+++ elpa/embark-20230211.2325/embark-pkg.el
@@ -0,0 +1,13 @@
+(define-package "embark" "20230211.2325" "Conveniently act on minibuffer completions"
+  '((emacs "27.1")
+    (compat "29.1.3.0"))
+  :commit "1f6c87d79f5d138cd128c230d727c796b4bc46cd" :authors
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainer
+  '("Omar Antolín Camarena" . "omar@matem.unam.mx")
+  :keywords
+  '("convenience")
+  :url "https://github.com/oantolin/embark")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
