1f33d6072b6b43b1e7fcad6f040543398cfd85a0
yulapshun <yulaphsun@gmail.com>
2023-03-06 21:39:07 +0100

    Update packages

---
+++ elpa/consult-20230305.729/consult-pkg.el
@@ -0,0 +1,11 @@
+(define-package "consult" "20230305.729" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.0"))
+  :commit "20c65446f3631636260ac319b7744af55e3377a1" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/org-roam-20230305.1720/org-roam-pkg.el
@@ -0,0 +1,16 @@
+(define-package "org-roam" "20230305.1720" "A database abstraction layer for Org-mode"
+  '((emacs "26.1")
+    (dash "2.13")
+    (org "9.4")
+    (emacsql "20230228")
+    (magit-section "3.0.0"))
+  :commit "b5436f3410d76206a0884a64769383dcebc85a1f" :authors
+  '(("Jethro Kuan" . "jethrokuan95@gmail.com"))
+  :maintainer
+  '("Jethro Kuan" . "jethrokuan95@gmail.com")
+  :keywords
+  '("org-mode" "roam" "convenience")
+  :url "https://github.com/org-roam/org-roam")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
