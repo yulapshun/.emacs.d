cbff0d0bb141ac47cb5ebb136a9e7b38afe5829c
yulapshun <yulapshun@gmail.com>
2023-03-25 13:21:30 +0100

    Add safe local variable values

--- custom.el
+++ custom.el
@@ -5,9 +5,34 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "969a67341a68becdccc9101dc87f5071b2767b75c0b199e0ded35bd8359ecd69" "521682d356435276b4bfb60cd134681aeaf4f2e4ee625456c04285da725ebf7c" default))
+ '(delete-selection-mode nil)
  '(package-selected-packages
    '(sqlite sqlite3 treesit-auto org-roam-ui cdlatex org-fragtog embark embark-consult orderless consult marginalia vertico org-roam org-roam-timestamps org nadvice which-key json-mode popper vterm avy benchmark-init esup dashboard undo-tree pyvenv pyvenv-auto ido-vertical-mode yasnippet-snippets company-box vundo yasnippet yaml-mode web-mode use-package symbol-overlay solarized-theme rjsx-mode pug-mode projectile php-mode paredit org-superstar neotree magit lsp-ui ido-completing-read+ gruvbox-theme go-mode git-gutter flycheck flx-ido eyebrowse exec-path-from-shell emojify elisp-benchmarks dockerfile-mode company-web company-flx auto-compile all-the-icons-dired))
- '(safe-local-variable-values '((org-pretty-entities)))
+ '(safe-local-variable-values
+   '((eval with-eval-after-load 'ox-latex
+           (add-to-list 'org-latex-classes
+                        '("tumarticle" "\\documentclass{tumarticle}"
+                          ("\\section{%s}" . "\\section*{%s}")
+                          ("\\subsection{%s}" . "\\subsection*{%s}")
+                          ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
+     (eval with-eval-after-load 'ox-latex
+           (add-to-list 'org-latex-classes
+                        '("tumbook" "\\documentclass{tumbook}"
+                          ("\\chapter{%s}" . "\\chapter*{%s}")
+                          ("\\section{%s}" . "\\section*{%s}")
+                          ("\\subsection{%s}" . "\\subsection*{%s}")
+                          ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))))
+     (eval add-to-list 'org-latex-classes
+           '("tumbook" "\\documentclass{tumbook}"
+             ("\\chapter{%s}" . "\\chapter*{%s}")
+             ("\\section{%s}" . "\\section*{%s}")
+             ("\\subsection{%s}" . "\\subsection*{%s}")
+             ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+     (org-export-with-section-numbers . t)
+     (org-export-with-toc . t)
+     (org-export-with-date . t)
+     (org-export-with-author . t)
+     (org-pretty-entities)))
  '(warning-suppress-types '((comp))))
 
 (custom-set-faces
