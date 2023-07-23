7e1ceb017bc07632b7d83095d4dcc05cf91a0ef9
yulapshun <yulapshun@gmail.com>
2023-07-23 11:31:16 +0200

    Mark theme as safe

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "969a67341a68becdccc9101dc87f5071b2767b75c0b199e0ded35bd8359ecd69" "521682d356435276b4bfb60cd134681aeaf4f2e4ee625456c04285da725ebf7c" default))
+   '("d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" "969a67341a68becdccc9101dc87f5071b2767b75c0b199e0ded35bd8359ecd69" "521682d356435276b4bfb60cd134681aeaf4f2e4ee625456c04285da725ebf7c" default))
  '(delete-selection-mode nil)
  '(package-selected-packages
    '(eglot sqlite sqlite3 treesit-auto org-roam-ui cdlatex org-fragtog embark embark-consult orderless consult marginalia vertico org-roam org-roam-timestamps org nadvice which-key json-mode popper vterm avy benchmark-init esup dashboard undo-tree pyvenv pyvenv-auto ido-vertical-mode yasnippet-snippets company-box vundo yasnippet yaml-mode web-mode use-package symbol-overlay solarized-theme rjsx-mode pug-mode projectile php-mode paredit org-superstar neotree magit lsp-ui ido-completing-read+ gruvbox-theme go-mode git-gutter flycheck flx-ido eyebrowse exec-path-from-shell emojify elisp-benchmarks dockerfile-mode company-web company-flx auto-compile all-the-icons-dired))
