79885234b23af3ca1eddf86280f816ec2a51ede8
yulapshun <yulapshun@gmail.com>
2023-07-31 20:38:26 +0200

    Optimize startup time

--- early-init.el
+++ early-init.el
@@ -6,11 +6,26 @@
 
 ;;; Code:
 
-(setq gc-cons-threshold most-positive-fixnum)
-(setq load-prefer-newer noninteractive)
+(defvar file-name-handler-alist-original file-name-handler-alist)
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6
+      file-name-handler-alist nil
+      site-run-file nil
+      load-prefer-newer noninteractive)
 
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq gc-cons-threshold (expt 2 23))))
+            (setq gc-cons-threshold 100000000
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist file-name-handler-alist-original)))
+
+(setq package-enable-at-startup nil)
+(setq load-prefer-newer noninteractive)
+(setq frame-inhibit-implied-resize t)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(when (featurep 'ns)
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
 
 ;;; early-init.el ends here
--- init.el
+++ init.el
@@ -8,13 +8,37 @@
 
 (defvar fast-init nil)
 
-(tool-bar-mode -1)
-(menu-bar-mode -1)
+;; Optimize startup, copied from Centaur Emacs
+(unless (or (daemonp) noninteractive init-file-debug)
+  ;; Prevent flashing of messages at startup
+  (when (display-graphic-p)
+    (setq-default inhibit-redisplay t
+                  inhibit-message t)
+    (defun reset-inhibit-vars ()
+      (setq-default inhibit-redisplay nil
+                    inhibit-message nil)
+      (redraw-frame))
+    (add-hook 'window-setup-hook #'reset-inhibit-vars)
+    (define-advice startup--load-user-init-file (:after (&rest _) reset-inhibit-vars)
+      (and init-file-had-error (reset-inhibit-vars))))
+
+  ;; Suppress file handlers operations at startup
+  ;; `file-name-handler-alist' is consulted on each call to `require' and `load'
+  (let ((old-value file-name-handler-alist))
+    (setq file-name-handler-alist nil)
+    (set-default-toplevel-value 'file-name-handler-alist file-name-handler-alist)
+    (add-hook 'emacs-startup-hook
+              (lambda ()
+                "Recover file name handlers."
+                (setq file-name-handler-alist
+                      (delete-dups (append file-name-handler-alist old-value))))
+              101)))
 
 (add-to-list 'load-path (expand-file-name "lib" user-emacs-directory))
 
-(if (or (<= emacs-major-version 26) fast-init)
-    (package-initialize))
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
