7b93d9cf5cab131a0a479409de8f95414a56a6d3
yulapshun <yulapshun@gmail.com>
2023-09-20 20:00:01 +0200

    Update packages

---
+++ elpa/auto-compile-20230901.1232/auto-compile-pkg.el
@@ -0,0 +1,14 @@
+(define-package "auto-compile" "20230901.1232" "Automatically compile Emacs Lisp libraries"
+  '((emacs "25.1"))
+  :commit "945f4ebf40e409688d58f003542b275eab6f83c5" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainers
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("compile" "convenience" "lisp")
+  :url "https://github.com/emacscollective/auto-compile")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
