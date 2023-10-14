e6f074e5fbe80daea1b6685ac4bf6115d87b6d85
yulapshun <yulapshun@gmail.com>
2023-10-14 15:34:29 +0200

    Update packages

---
+++ elpa/mood-line-20231013.2108/mood-line-pkg.el
@@ -0,0 +1,14 @@
+(define-package "mood-line" "20231013.2108" "A minimal mode line inspired by doom-modeline"
+  '((emacs "26.1"))
+  :commit "060959f5e24468896ba04502a2f8ba8dcff0cb3b" :authors
+  '(("Jessie Hildebrandt <jessieh.net>"))
+  :maintainers
+  '(("Jessie Hildebrandt <jessieh.net>"))
+  :maintainer
+  '("Jessie Hildebrandt <jessieh.net>")
+  :keywords
+  '("mode-line" "faces")
+  :url "https://gitlab.com/jessieh/mood-line")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
