d97dd3cedcd91d5cf3484287750be42edc85efc8
yulapshun <yulapshun@gmail.com>
2023-11-16 15:33:40 +0100

    Update packages

---
+++ elpa/consult-20231115.1746/consult-pkg.el
@@ -0,0 +1,15 @@
+(define-package "consult" "20231115.1746" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.1"))
+  :commit "d7a0415c4ff3a84b696e5ddbc2ef15d0d45c405d" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("matching" "files" "completion")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
