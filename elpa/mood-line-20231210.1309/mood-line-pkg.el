87d428156163a0fa97ba1ae464553deabf7c064b
yulapshun <yulapshun@gmail.com>
2023-12-17 13:30:33 +0100

    Update packages

---
+++ elpa/mood-line-20231210.1309/mood-line-pkg.el
@@ -0,0 +1,14 @@
+(define-package "mood-line" "20231210.1309" "A minimal mode line inspired by doom-modeline"
+  '((emacs "26.1"))
+  :commit "d1c024fdf9543fbc0101cd2c6e8b248378f591cd" :authors
+  '(("Jessie Hildebrandt <jessieh.net>"))
+  :maintainers
+  '(("Jessie Hildebrandt <jessieh.net>"))
+  :maintainer
+  '("Jessie Hildebrandt <jessieh.net>")
+  :keywords
+  '("mode-line" "faces")
+  :url "https://gitlab.com/jessieh/mood-line")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/vertico-prescient-20231123.2208/vertico-prescient-pkg.el
@@ -0,0 +1,16 @@
+(define-package "vertico-prescient" "20231123.2208" "prescient.el + Vertico"
+  '((emacs "27.1")
+    (prescient "6.1.0")
+    (vertico "0.28"))
+  :commit "c0eca3328313f1e93d24e686307516f43a484ba2" :authors
+  '(("Radian LLC" . "contact+prescient@radian.codes"))
+  :maintainers
+  '(("Radian LLC" . "contact+prescient@radian.codes"))
+  :maintainer
+  '("Radian LLC" . "contact+prescient@radian.codes")
+  :keywords
+  '("extensions")
+  :url "https://github.com/radian-software/prescient.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
