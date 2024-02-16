92d77c79558897b7c60f8a65d7caddad3b139dc8
yulapshun <yulapshun@gmail.com>
2024-02-16 10:01:08 +0100

    Update packages

---
+++ elpa/consult-20240209.2104/consult-pkg.el
@@ -0,0 +1,15 @@
+(define-package "consult" "20240209.2104" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "8cb56f8d6637e55340271274a20dfc163d77fff5" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("matching" "files" "completion")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/embark-20240131.1248/embark-pkg.el
@@ -0,0 +1,15 @@
+(define-package "embark" "20240131.1248" "Conveniently act on minibuffer completions"
+  '((emacs "27.1")
+    (compat "29.1.4.0"))
+  :commit "99484b04ec72b9febfb2175b075ba55e40bcbf72" :authors
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainers
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
+  :maintainer
+  '("Omar Antolín Camarena" . "omar@matem.unam.mx")
+  :keywords
+  '("convenience")
+  :url "https://github.com/oantolin/embark")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
