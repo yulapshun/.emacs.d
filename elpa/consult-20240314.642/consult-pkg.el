c63f50b1a2f00398076f06ee3c07052278b3e22a
yulapshun <yulapshun@gmail.com>
2024-03-19 20:01:12 +0100

    Update packages

---
+++ elpa/consult-20240314.642/consult-pkg.el
@@ -0,0 +1,15 @@
+(define-package "consult" "20240314.642" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "b48ff6bf0527baeb6bfd07c6da9d303ff0b79c3d" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("matching" "files" "completion")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/treesit-auto-20240223.1450/treesit-auto-pkg.el
@@ -0,0 +1,14 @@
+(define-package "treesit-auto" "20240223.1450" "Automatically use tree-sitter enhanced major modes"
+  '((emacs "29.0"))
+  :commit "299dd88c9e5f1ff91a8f1989ad1e97e86c220020" :authors
+  '(("Robb Enzmann" . "robbenzmann@gmail.com"))
+  :maintainers
+  '(("Robb Enzmann" . "robbenzmann@gmail.com"))
+  :maintainer
+  '("Robb Enzmann" . "robbenzmann@gmail.com")
+  :keywords
+  '("treesitter" "auto" "automatic" "major" "mode" "fallback" "convenience")
+  :url "https://github.com/renzmann/treesit-auto.git")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
