5372a635f0b0f390316929aeacf717c47cbea33f
yulapshun <yulapshun@gmail.com>
2024-06-15 12:08:46 +0200

    Update packages

---
+++ elpa/gruvbox-theme-20240615.432/gruvbox-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "gruvbox-theme" "20240615.432" "A retro-groove colour theme for Emacs"
+  '((autothemer "0.2"))
+  :commit "d2404eb157845536b111999a4332d58a4867427e" :authors
+  '(("Jason Milkins" . "jasonm23@gmail.com"))
+  :maintainers
+  '(("Jason Milkins" . "jasonm23@gmail.com"))
+  :maintainer
+  '("Jason Milkins" . "jasonm23@gmail.com")
+  :url "https://github.com/greduan/emacs-theme-gruvbox")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/php-mode-20240527.1834/php-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "php-mode" "20240527.1834" "Major mode for editing PHP code"
+  '((emacs "26.1"))
+  :commit "c8e4c16ca0d234f4a659713a94467f0c32ef4bc8" :authors
+  '(("Eric James Michael Ritz"))
+  :maintainers
+  '(("USAMI Kenta" . "tadsan@zonu.me"))
+  :maintainer
+  '("USAMI Kenta" . "tadsan@zonu.me")
+  :keywords
+  '("languages" "php")
+  :url "https://github.com/emacs-php/php-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
