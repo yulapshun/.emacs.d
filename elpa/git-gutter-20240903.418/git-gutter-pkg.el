1d1d50be95ba44cccd9ead849e6ee9f1399edd3b
yulapshun <yulapshun@gmail.com>
2024-10-15 10:48:00 +0200

    Update packages

---
+++ elpa/git-gutter-20240903.418/git-gutter-pkg.el
@@ -0,0 +1,13 @@
+(define-package "git-gutter" "20240903.418" "Port of Sublime Text plugin GitGutter"
+  '((emacs "25.1"))
+  :commit "9beaed9e45f92ab0527f54dbf5fb241b84e7821d" :authors
+  '(("Syohei YOSHIDA" . "syohex@gmail.com"))
+  :maintainers
+  '(("Neil Okamoto" . "neil.okamoto+melpa@gmail.com")
+    ("Jen-Chieh" . "jcs090218@gmail.com"))
+  :maintainer
+  '("Neil Okamoto" . "neil.okamoto+melpa@gmail.com")
+  :url "https://github.com/emacsorphanage/git-gutter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/symbol-overlay-20240913.1624/symbol-overlay-pkg.el
@@ -0,0 +1,15 @@
+(define-package "symbol-overlay" "20240913.1624" "Highlight symbols with keymap-enabled overlays"
+  '((emacs "24.3")
+    (seq "2.2"))
+  :commit "6151f4279bd94b5960149596b202cdcb45cacec2" :authors
+  '(("wolray" . "wolray@foxmail.com"))
+  :maintainers
+  '(("wolray" . "wolray@foxmail.com"))
+  :maintainer
+  '("wolray" . "wolray@foxmail.com")
+  :keywords
+  '("faces" "matching")
+  :url "https://github.com/wolray/symbol-overlay/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
