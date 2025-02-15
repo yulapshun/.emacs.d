65c25804fc623eeebfb4a1078a8d641a82362cca
yulapshun <yulapshun@gmail.com>
2025-02-15 10:53:22 +0100

    Update packages

--- elpa/gruvbox-theme-20240615.432/gruvbox-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "gruvbox-theme" "20240615.432"
-  "A retro-groove colour theme for Emacs."
-  '((autothemer "0.2"))
-  :url "https://github.com/greduan/emacs-theme-gruvbox"
-  :commit "d2404eb157845536b111999a4332d58a4867427e"
-  :revdesc "d2404eb15784"
-  :authors '(("Jason Milkins" . "jasonm23@gmail.com"))
-  :maintainers '(("Jason Milkins" . "jasonm23@gmail.com")))
---
+++ elpa/transient-20250213.1526/transient-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transient" "20250213.1526"
+  "Transient commands."
+  '((emacs  "26.1")
+    (compat "30.0.0.0")
+    (seq    "2.24"))
+  :url "https://github.com/magit/transient"
+  :commit "58e22554ab85e3bed6dd84b9821a7e9760dc9775"
+  :revdesc "58e22554ab85"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
