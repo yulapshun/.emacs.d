1bc811ce9840ae06795e77253f5f063bd7a33474
yulapshun <yulapshun@gmail.com>
2025-03-15 20:20:33 +0100

    UPdate packages

--- elpa/compile-angel-20250211.1704/compile-angel-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "compile-angel" "20250211.1704"
-  "Automatically Compile Elisp files (auto-compile alternative)."
-  '((emacs "26.1"))
-  :url "https://github.com/jamescherti/compile-angel.el"
-  :commit "333e943b12754bfe844f9d2bbc8a6f4d54503ad2"
-  :revdesc "333e943b1275"
-  :keywords '("convenience"))
---
+++ elpa/transient-20250312.1453/transient-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transient" "20250312.1453"
+  "Transient commands."
+  '((emacs  "26.1")
+    (compat "30.0.0.0")
+    (seq    "2.24"))
+  :url "https://github.com/magit/transient"
+  :commit "4a06aeb0f7bd279f76bdf619a7852a4725bf4cc5"
+  :revdesc "4a06aeb0f7bd"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
