abbad0003b5f4f0da725dfb187503a1e29fc451d
Lap Shun Yu <yulapshun@gmail.com>
2025-03-30 14:59:48 +0200

    Update gptel

---
+++ elpa/gptel-20250330.341/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20250330.341"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.7.4")
+    (compat    "29.1.4.1"))
+  :url "https://github.com/karthink/gptel"
+  :commit "35d53cada3e1fc323b6a18739368e85a82a9e419"
+  :revdesc "35d53cada3e1"
+  :keywords '("convenience" "tools")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
