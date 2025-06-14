fa0d9737fd65e14c3278925d33928724871f6af0
Lap Shun Yu <yulapshun@gmail.com>
2025-06-14 15:33:05 -0400

    Update packages

---
+++ elpa/compile-angel-20250611.317/compile-angel-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "compile-angel" "20250611.317"
+  "Automatically Compile Elisp files (auto-compile alternative)."
+  '((emacs "26.3"))
+  :url "https://github.com/jamescherti/compile-angel.el"
+  :commit "d8b0a801828772563e409ecc06d4e20d4522b6a1"
+  :revdesc "d8b0a8018287"
+  :keywords '("convenience"))
--- elpa/racket-mode-20250514.1427/racket-mode-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "racket-mode" "20250514.1427"
-  "Racket editing, REPL, and more."
-  '((emacs  "25.1")
-    (compat "30.0.20.0"))
-  :url "https://www.racket-mode.com/"
-  :commit "1ec8ac5341ba9f9b70de02a8397fa02297594c16"
-  :revdesc "1ec8ac5341ba"
-  :authors '(("Greg Hendershott" . "racket-mode-author@greghendershott.com")))
