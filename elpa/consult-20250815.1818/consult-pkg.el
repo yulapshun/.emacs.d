9366ec2e4a899c03b27ee7cb297852a693f4082b
Lap Shun Yu <yulapshun@gmail.com>
2025-08-16 14:01:37 +0200

    Update packages

---
+++ elpa/consult-20250815.1818/consult-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "consult" "20250815.1818"
+  "Consulting completing-read."
+  '((emacs  "28.1")
+    (compat "30"))
+  :url "https://github.com/minad/consult"
+  :commit "48920075058421dde0592c7702ed84e9fa2ffb5d"
+  :revdesc "489200750584"
+  :keywords '("matching" "files" "completion")
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
