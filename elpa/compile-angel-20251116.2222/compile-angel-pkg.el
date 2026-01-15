712e00b1df7929a5362346fd3e74625b79f05b45
Lap Shun Yu <yulapshun@gmail.com>
2026-01-15 20:28:50 +0100

    Update packages

--- elpa/compile-angel-20251116.2222/compile-angel-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "compile-angel" "20251116.2222"
-  "Automatically Compile Elisp files (auto-compile alternative)."
-  '((emacs "26.3"))
-  :url "https://github.com/jamescherti/compile-angel.el"
-  :commit "95a0d01dc9c6eeac5023465fadf8ab82726df097"
-  :revdesc "95a0d01dc9c6"
-  :keywords '("convenience"))
