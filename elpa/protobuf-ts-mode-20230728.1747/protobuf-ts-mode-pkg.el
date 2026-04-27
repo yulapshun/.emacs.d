a708878197b09f3bf6d401010752ac00e6bb240b
Lap Shun Yu <yulapshun@gmail.com>
2026-04-27 09:53:41 +0200

    Install protobuf-mode

---
+++ elpa/protobuf-ts-mode-20230728.1747/protobuf-ts-mode-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "protobuf-ts-mode" "20230728.1747"
+  "Tree sitter support for Protocol Buffers (proto3 only)."
+  '((emacs "29"))
+  :url "https://git.ookami.one/cgit/protobuf-ts-mode"
+  :commit "65152f5341ea4b3417390b3e60b195975161b8bc"
+  :revdesc "65152f5341ea"
+  :keywords '("protobuf" "languages" "tree-sitter")
+  :authors '(("ookami" . "mail@ookami.one"))
+  :maintainers '(("ookami" . "mail@ookami.one")))
